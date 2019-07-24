d84e22ce75f12845d742be4924eb6a7b9fae9e63
Graham Christensen <graham@grahamc.com>
2019-07-24 16:10:28 -0700

    Disk import: ignore zpools

--- system.nix
+++ system.nix
@@ -7,7 +7,7 @@ let
       set -eux
       set -o pipefail
 
-      ${pkgs.utillinux}/bin/lsblk -d -e 1,7,11 -o NAME -n \
+      ${pkgs.utillinux}/bin/lsblk -d -e 1,7,11,230 -o NAME -n \
         | ${pkgs.busybox}/bin/sed -e "s#^#/dev/#" \
         | ${pkgs.busybox}/bin/xargs ${pkgs.zfs}/bin/zpool \
             create -f \
