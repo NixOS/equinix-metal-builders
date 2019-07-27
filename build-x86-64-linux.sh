043f4fe0dfcbaca734b576ef9320ad49153d5abb
Graham Christensen <graham@grahamc.com>
2019-07-27 05:15:01 -0700

    x86 build: use-substitutes on the remote end

--- build-x86-64-linux.sh
+++ build-x86-64-linux.sh
@@ -6,5 +6,5 @@ set -o pipefail
 
 drv=$(realpath $(time nix-instantiate ./instances/m2.xlarge.x86.nix --show-trace --add-root ./result.drv --indirect))
 
-nix-copy-closure --to "netboot@flexo.gsc.io" "$drv"
+nix-copy-closure --use-substitutes --to "netboot@flexo.gsc.io" "$drv"
 ssh $NIX_SSHOPTS "netboot@flexo.gsc.io" NIX_REMOTE=daemon nix-store --realize "$drv" --add-root /var/lib/nginx/netboot/netboot.gsc.io/hydra-m2.xlarge.x86 --indirect --keep-going
