e87ae60b7f03ba8a2f80a9a1b06700f9c08ddea9
Graham Christensen <graham@grahamc.com>
2019-08-01 14:19:51 -0400

    Disable magic auto gc

--- instances/c2.large.arm.nix
+++ instances/c2.large.arm.nix
@@ -22,11 +22,14 @@
         gbFree = 100;
       in "--max-freed $((${toString gbFree} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | awk '{ print $4 }')))";
 
+      /*
+      # Buggy as of 2019-08-01 https://nix-cache.s3.amazonaws.com/log/5gkp2g9l56hy9jzdl6qmrxgmjp7sz36z-rustc-1.36.0.drv
       # If we drop below 40G, free 100G
       extraOptions = ''
         min-free = ${toString (40*1024*1024*1024)}
         max-free = ${toString (100*1024*1024*1024)}
       '';
+      */
     };
   };
 }
