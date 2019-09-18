ca5cb166a1313ea902cd35d893cbceec65f2099e
Graham Christensen <graham@grahamc.com>
2019-09-18 12:31:52 -0400

    instances: fix nix-collect-garbage's calculation

--- instances/c2.large.arm.nix
+++ instances/c2.large.arm.nix
@@ -1,7 +1,7 @@
 (import ../make-netboot.nix)
 {
   buildSystem = "aarch64-linux";
-  hardware = {
+  hardware = { pkgs, ... }: {
     nixpkgs.system = "aarch64-linux";
     boot = {
       kernelParams = [
@@ -20,7 +20,7 @@
       buildCores = 2;
       gc.options = let
         gbFree = 100;
-      in "--max-freed $((${toString gbFree} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | awk '{ print $4 }')))";
+      in ''--max-freed "$((${toString gbFree} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
 
       /*
       # Buggy as of 2019-08-01 https://nix-cache.s3.amazonaws.com/log/5gkp2g9l56hy9jzdl6qmrxgmjp7sz36z-rustc-1.36.0.drv
