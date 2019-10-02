c3f4beadd74bde5fc872da3386a8fb34180ebfe9
Graham Christensen <graham@grahamc.com>
2019-10-02 09:59:14 -0400

    rolling-reboot: show console logs, retry reboot if it doesn't boot after 30min

--- rolling-reboot.sh
+++ rolling-reboot.sh
@@ -42,16 +42,31 @@ ids_to_reboot() {
 ' --arg plan "$PLAN"
 }
 
-for id in $(ids_to_reboot); do
-    echo " ═> Rebooting ${id}..."
+sos() {
+    curl \
+        --header 'Accept: application/json' \
+        --header 'Content-Type: application/json' \
+        --header "X-Auth-Token: $PACKET_TOKEN" \
+        --fail \
+        "https://api.packet.net/devices/${1}" \
+        | jq -r '
+.id + "@sos." + .facility.code + ".packet.net"
+'
+}
+
+reboot() {
     curl \
         --data '{"type": "reboot"}' \
         --header 'Accept: application/json' \
         --header 'Content-Type: application/json' \
         --header "X-Auth-Token: $PACKET_TOKEN" \
         --fail \
-        "https://api.packet.net/devices/${id}/actions"
+        "https://api.packet.net/devices/${1}/actions"
+}
 
+for id in $(ids_to_reboot); do
+    echo " ═> Rebooting ${id}..."
+    reboot "${id}"
     host=$(echo "$id" | cut -d- -f1 | sed -e 's/$/.packethost.net/')
     echo -n " ├─ waiting for ${id} to go down"
     while [ $(ssh-keyscan "$host" 2> /dev/null | wc -l) -gt 0 ] ; do
@@ -60,10 +75,34 @@ for id in $(ids_to_reboot); do
     echo ""
     echo " │    ... down!"
 
-    echo -n " ├─ waiting for ${id} to come back up"
-    while [ $(ssh-keyscan "$host" 2> /dev/null  | wc -l) -eq 0 ] ; do
-        echo -n "."
+    echo " ├─ waiting for ${id} to come back up"
+
+    up=0
+    last_keyscan=$(date +%s)
+    keyscans_remaining=60
+    coproc SSH (ssh "$(sos "${id}")" 2>&1 | while read -r line; do printf " │         %s  %s\n" "$(date)" "$line"; done)
+    while [ $up -eq 0 ]; do
+        if read -t5 -r output <&"${SSH[0]}"; then
+            echo "$output"
+        fi
+
+        now=$(date +%s)
+        if [ $((now - last_keyscan)) -gt 30 ]; then
+            last_keyscan=$now
+            if [ $(ssh-keyscan "$host" 2> /dev/null  | wc -l) -eq 0 ] ; then
+                echo -n "."
+                keyscans_remaining=$((keyscans_remaining - 1))
+                if [ $keyscans_remaining -eq 0 ]; then
+                    reboot "${id}"IRC li
+                    keyscans_remaining=60
+                fi
+            else
+                up=1
+            fi
+        fi
     done
+    kill "${SSH_PID}"
+
     echo ""
     echo " │    ... up!"
     echo " ├─ testing remote Nix builds"
