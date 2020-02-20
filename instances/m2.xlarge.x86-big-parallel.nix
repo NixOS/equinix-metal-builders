56500d942bb4568d869dbdd000795f2004a78c15
Graham Christensen <graham@grahamc.com>
2020-02-19 21:15:57 -0500

    Commit hardware specs for {c2.large.arm,m2.xlarge.x86}-big-parallel

---
+++ instances/m2.xlarge.x86-big-parallel.nix
@@ -0,0 +1,33 @@
+(import ../make-netboot.nix)
+{
+  buildSystem = "x86_64-linux";
+  hardware = { pkgs, ... }: {
+    nixpkgs.system = "x86_64-linux";
+    boot = {
+      kernelModules = [ "kvm-intel" ];
+      kernelParams = [ "console=ttyS1,115200n8" "initrd=initrd" ];
+      initrd = {
+        availableKernelModules = [
+          "ahci" "xhci_pci" "mpt3sas" "nvme" "sd_mod"
+        ];
+      };
+    };
+
+    nix = {
+      maxJobs = 2;
+      buildCores = 48;
+      gc.options = let
+        gbFree = 100;
+      in ''--max-freed "$((${toString gbFree} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
+
+      /*
+      # Buggy as of 2019-08-01 https://nix-cache.s3.amazonaws.com/log/5gkp2g9l56hy9jzdl6qmrxgmjp7sz36z-rustc-1.36.0.drv
+      # If we drop below 40G, free 100G
+      extraOptions = ''
+        min-free = ${toString (40*1024*1024*1024)}
+        max-free = ${toString (100*1024*1024*1024)}
+      '';
+      */
+    };
+  };
+}
