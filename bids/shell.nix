e506c3f7ab09e6404c2299dcf2b01a2e5e1001fd
Graham Christensen <graham@grahamc.com>
2021-01-10 17:24:16 -0500

    Add terraform config for managing bids

---
+++ bids/shell.nix
@@ -0,0 +1,25 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  buildInputs = [
+    pkgs.jq
+    (pkgs.terraform_0_12.withPlugins (p: [
+      (pkgs.buildGoPackage rec {
+        name = "terraform-provider-metal";
+        version = "1.0.0";
+        goPackagePath = "github.com/equinix/terraform-provider-metal";
+        subPackages = [ "." ];
+        src = pkgs.fetchFromGitHub {
+          owner = "equinix";
+          repo = "terraform-provider-metal";
+          rev = "v1.0.0";
+          sha256 = "sha256-wA3L0SEDWyU5OwrK+5W59Be9hNaC/gahu1fBaU5xmt4=";
+        };
+        # Terraform allow checking the provider versions, but this breaks
+        # if the versions are not provided via file paths.
+        postBuild = "mv go/bin/terraform-provider-metal{,_v1.0.0}";
+      })
+    ]))
+  ];
+}
+
