b350a6f72031e8f11c1452e9d1a1afcc01f1ab9f
Graham Christensen <graham@grahamc.com>
2022-06-05 20:22:32 -0400

    Give nix-daemon a higher open file limit

--- nix.nix
+++ nix.nix
@@ -34,6 +34,8 @@ in {
 
   config = mkMerge [
     {
+    
+      systemd.services.nix-daemon.serviceConfig.LimitNOFILE = 16384;
       nix.gc.options = ''--max-freed "$((${toString config.nix.gbFree} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
     }
     (mkIf config.nix.makeAbout {
